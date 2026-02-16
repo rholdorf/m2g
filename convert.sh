dea22a4d9dd59ca9b487d3321dcca6b6611f8832
Rui Holdorf <rui.h@me.com>
2026-02-15 22:54:49 -0300

    add more anims

--- convert.sh
+++ convert.sh
@@ -1,21 +1,21 @@
 ## com inplace
-/Applications/Blender.app/Contents/MacOS/Blender -b -P ./mixamo_batch.py -- \
-   --character ./input/x-bot.fbx \
-   --anims \
-   ./input/anims/idle.fbx \
-   --out ./output \
-   --mode inplace \
-   --inplace-ground-lock true \
-   --export-mesh-in-anims false \
-   --apply-scale true \
-   --fix-root true \
-   --limit-weights 4 \
-   --abort-on-incompatible false
+#/Applications/Blender.app/Contents/MacOS/Blender -b -P ./mixamo_batch.py -- \
+#   --character ./input/x-bot.fbx \
+#   --anims \
+#   --out ./output \
+#   --mode inplace \
+#   --inplace-ground-lock true \
+#   --export-mesh-in-anims false \
+#   --apply-scale true \
+#   --fix-root true \
+#   --limit-weights 4 \
+#   --abort-on-incompatible false
 
 ## Sem inplace
 /Applications/Blender.app/Contents/MacOS/Blender -b -P ./mixamo_batch.py -- \
    --character ./input/x-bot.fbx \
    --anims \
+   ./input/anims/idle.fbx \
    ./input/anims/dying.fbx \
    ./input/anims/look_around.fbx \
    ./input/anims/ninja_idle.fbx \
@@ -32,6 +32,11 @@
 /Applications/Blender.app/Contents/MacOS/Blender -b -P ./mixamo_batch.py -- \
    --character ./input/x-bot.fbx \
    --anims \
+   ./input/anims/right_strafe.fbx \
+   ./input/anims/right_strafe_walk.fbx \
+   ./input/anims/jumping.fbx \
+   ./input/anims/left_strafe_walk.fbx \
+   ./input/anims/left_strafe.fbx \
    ./input/anims/catwalk_walk_forward.fbx \
    ./input/anims/walking.fbx \
    ./input/anims/running.fbx \
